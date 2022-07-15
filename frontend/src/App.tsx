65da7717ff52f6c5eba470cd68e166115384f8ee
lucimarNeves <lucimar.neves@gmail.com>
2022-07-14 22:10:16 -0300

    Project created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
+
+
+function App() {
+  
+  return (
+
+    <>
+      <h1>Lucimar</h1>
+      <NotificationButton />
+    </>
+  )
+  
+}
+
+export default App
