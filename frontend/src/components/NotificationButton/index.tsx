8e9eacac9d9f5b92e9e5f05d027bf61d4b848c0e
lucimarNeves <lucimar.neves@gmail.com>
2022-07-18 16:48:46 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './style.css'
@@ -12,7 +13,8 @@ function handleClick(id : number ){
   
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response =>  {
-    console.log("Sucesso")
+    //console.log("Sucesso")
+    toast.info("SMS enviado com sucesso");
   })
 
 }
